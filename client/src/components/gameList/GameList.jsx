12c985dcc5f073e8a8ec7481522f6ac4d0e73e7e
Amunra <hungnguyentrong285@gmail.com>
2024-11-01 08:17:00 +0700

    Pagination

--- client/src/components/gameList/GameList.jsx
+++ client/src/components/gameList/GameList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Pagination from '../Pagination/Pagination';
 import './GameList.scss';
 
 const GameList = () => {
   const [games, setGames] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const gamesPerPage = 4;
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -12,7 +15,7 @@ const GameList = () => {
       try {
         const response = await axios.get('http://localhost:8800/api/game');
         if (response.data.success) {
-          setGames(response.data.data); // Gán response.data.data vào state games
+          setGames(response.data.data);
         } else {
           console.error('Failed to fetch games:', response.data.message);
         }
@@ -21,23 +24,48 @@ const GameList = () => {
       }
     };
 
-    fetchGames(); // Gọi hàm fetchGames
+    fetchGames();
   }, []);
 
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+
   const handleGameClick = (gameId) => {
-    navigate(`/game/${gameId}`); // Chuyển hướng đến trang chi tiết game
+    navigate(`/game/${gameId}`);
   };
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(games.length / gamesPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const totalPages = Math.ceil(games.length / gamesPerPage);
 
   return (
     <div className='gamelistcontainer'>
       <div className="detailgamelist">
-        {games.map(game => (
+        {currentGames.map(game => (
           <div className="game" key={game.GameID} onClick={() => handleGameClick(game.GameID)}>
             {game.Image && <img src={game.Image} alt={game.Name} />}
             <h2>{game.Name}</h2>
-          </div>  
+          </div>
         ))}
       </div>
+      
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onNextPage={handleNextPage}
+        onPrevPage={handlePrevPage}
+      />
     </div>
   );
 };
