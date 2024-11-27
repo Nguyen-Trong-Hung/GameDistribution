11ceca3bd6e5175e20805bd399bf0cfb09c3110c
Amunra <hungnguyentrong285@gmail.com>
2024-11-27 15:12:37 +0700

    Update PORT

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ import commentsRoute from './Routes/comments.route.js';
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:1702', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
